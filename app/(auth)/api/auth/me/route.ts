f1e3fe2194e8834eb61d072b82dce6065f0cbb73
KiNG <odon66674@gmail.com>
2025-05-11 19:14:53 +0200

    add some update package

--- app/(auth)/api/auth/me/route.ts
+++ app/(auth)/api/auth/me/route.ts
@@ -1,13 +1,13 @@
-// app/api/user/me/route.ts
-import { NextResponse } from "next/server";
-import { getUserFromToken } from "@/lib/auth";
+// // app/api/user/me/route.ts
+// import { NextResponse } from "next/server";
+// import { getUserFromToken } from "@/lib/auth";
 
-export async function GET(req: Request) {
-  const user = await getUserFromToken(req);
+// export async function GET(req: Request) {
+//   const user = await getUserFromToken(req);
 
-  if (!user) {
-    return new NextResponse("Unauthorized", { status: 401 });
-  }
+//   if (!user) {
+//     return new NextResponse("Unauthorized", { status: 401 });
+//   }
 
-  return NextResponse.json(user);
-}
+//   return NextResponse.json(user);
+// }
